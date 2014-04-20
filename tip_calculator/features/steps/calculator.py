224c73da0465525e629818dd5fdb46d7d3f1b4d8
adrian.frimpong <adrian.frimpong@gmail.com>
2014-04-20 15:20:04 -0400

    Added form features, using Selenium to test browser context

---
+++ tip_calculator/features/steps/calculator.py
@@ -0,0 +1,24 @@
+@when(u'I go to the tip calculator')
+def step_impl(context):
+	context.browser.get('http://localhost:5000')
+
+@then(u'I should see the calculator form')
+def step_impl(context):
+	assert "Tip calculator" in context.browser.title
+
+@when(u'I submit the form with a valid total and tip percentage')
+def step_impl(context):
+	br = context.browser
+	br.get('http://localhost:5000')
+	meal_cost = br.find_element_by_name("meal_cost")
+	meal_cost.send_keys("30")
+	tip_percentage = br.find_element_by_name("tip_percentage")
+	tip_percentage.send_keys("20")
+	br.find_element_by_name("submit").click()
+
+@then('I should see the results page')
+def step_impl(context):
+	br = context.browser
+	assert br.find_element_by_id('results')
+	
+
