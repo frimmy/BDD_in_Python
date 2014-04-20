1bad39e721779ea59da1464ac8f7ff766d250cf0
adrian.frimpong <adrian.frimpong@gmail.com>
2014-04-20 16:10:55 -0400

    Added results template, results route to app.py, and modified the home layout template

--- tip_calculator/app.py
+++ tip_calculator/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+@app.route('/results', methods=['POST'])
+def results():
+	return render_template('results.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
